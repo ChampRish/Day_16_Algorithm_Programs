51b46fac66177d1d6a45f703fef390cccc56e3df
RISHABH <gguptarishabh82@gmail.com>
2022-03-23 18:40:27 +0530

    Take 2 Strings as Input such abcd and dcba and Check for Anagrams

---
+++ UC6_AnagramsProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Programs_Day16_17
+{
+    internal class UC6_AnagramsProgram
+    {
+        public void AnagramsProgram()
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Both the strings are Anagrams");
+            }
+            else
+            {
+                Console.WriteLine("Both the strings are not Anagrams");
+            }
+        }
+    }
+}
